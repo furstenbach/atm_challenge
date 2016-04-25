1d1de2b21d5bf397886462ed7cdfc42ef43bd427
Jenny <jenny.furstenbach@gmail.com>
2016-04-25 12:27:50 +0000

    finished step 3

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,15 +1,31 @@
 class Atm
-    
     attr_accessor :funds
     
     def initialize
         @funds = 1000
     end
     
-    def withdraw(amount)
-        @funds -= amount
+    def withdraw(amount, account)
+        case
+        when insufficient_funds_in_account(amount, account) then
+            return
+        else
+            perform_transaction(amount, account)
     end
     
+end 
+
+private
+    
+    def insufficient_funds_in_account(amount, account)
+        amount > account.balance 
+    end
+    
+    def perform_transaction(amount, account)
+        @funds -= amount
+        account.balance = account.balance - amount
+        { status: true, message: 'success', date: Date.today, amount: amount }
+    end
 end
 
 
