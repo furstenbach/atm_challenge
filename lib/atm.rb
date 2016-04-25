cc37f42f8400a9ef1da924f1746f771c3e3ac9a7
Jenny <jenny.furstenbach@gmail.com>
2016-04-25 14:22:57 +0000

    finished step 4

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,12 +5,18 @@ class Atm
         @funds = 1000
     end
     
-    def withdraw(amount, account)
+    def withdraw(amount, pin_code,account_status, account)
         case
         when insufficient_funds_in_account(amount, account) then
             { status: false, message: 'insufficient funds', date: Date.today }
         when insufficient_funds_in_atm(amount) then
-            {status: false, message: 'insufficient funds in ATM', date: Date.today }
+            { status: false, message: 'insufficient funds in ATM', date: Date.today }
+        when incorrect_pin(pin_code, account.pin_code) then
+            { status: false, message: 'wrong pin', date: Date.today }
+        when card_expired(account.exp_date) then
+            {status: false, message: 'card expired', date: Date.today }
+        when account_disabled(account_status, account.account_status) then
+            { status: false, message: 'account disabled', date: Date.today }
         else
             perform_transaction(amount, account)
     end
@@ -27,14 +33,24 @@ private
         @funds < amount  
     end
     
+    def incorrect_pin(pin_code, actual_pin)
+        pin_code != actual_pin
+    end
+    
+     def card_expired(exp_date)
+        Date.strptime(exp_date, '%m/%y') < Date.today
+    end
+    
+    def account_disabled(account_status, actual_account_status) 
+        account_status != actual_account_status
+    end    
+    
     def perform_transaction(amount, account)
         @funds -= amount
         account.balance = account.balance - amount
         { status: true, message: 'success', date: Date.today, amount: amount }
     end
-    
-    
-    
+
 end
 
 
